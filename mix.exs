c9c25b34bb60cd57acd3c08002f8489d97a951a5
Xavier Defrang <xavier.defrang@gmail.com>
2019-11-18 19:10:36 +0100

    Add ex_doc

--- mix.exs
+++ mix.exs
@@ -26,7 +26,8 @@ defmodule XlsxReader.MixProject do
     [
       {:saxy, "~> 0.10"},
       {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0.0-rc.7", only: [:dev], runtime: false}
+      {:dialyxir, "~> 1.0.0-rc.7", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false}
     ]
   end
 end
