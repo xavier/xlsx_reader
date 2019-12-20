c98f9efe3ba872ec0fe26cd560b2e4bbbc9f4fb2
Xavier Defrang <xavier.defrang@gmail.com>
2019-12-20 15:29:30 +0100

    Release 0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## [0.1.1]
+
+- Improve handling of whitespace in shared strings
+
 ## [0.1.0]
 
 - Initial release
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule XlsxReader.MixProject do
   def project do
     [
       app: :xlsx_reader,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
