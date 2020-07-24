1bf7e8e46585684d43c0ca4372e3426728e168a4
Xavier Defrang <xavier.defrang@gmail.com>
2020-07-24 22:02:46 +0200

    Add test for README install version

--- test/xlsx_reader_test.exs
+++ test/xlsx_reader_test.exs
@@ -1,6 +1,24 @@
 defmodule XlsxReaderTest do
   use ExUnit.Case
 
+  test "README install version check" do
+    app = :xlsx_reader
+
+    app_version = "#{Application.spec(app, :vsn)}"
+    readme = File.read!("README.md")
+    [_, readme_versions] = Regex.run(~r/{:#{app}, "(.+)"}/, readme)
+
+    assert Version.match?(
+             app_version,
+             readme_versions
+           ),
+           """
+           Install version constraint in README.md does not match to current app version.
+           Current App Version: #{app_version}
+           Readme Install Versions: #{readme_versions}
+           """
+  end
+
   describe "open/2" do
     test "opens a xlsx file from the file system" do
       xlsx = TestFixtures.path("test.xlsx")
