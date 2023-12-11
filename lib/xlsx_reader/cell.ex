3ea8890647a25d2566afe2c0689fcb6532ff0e2d
dylan <dylan.harness@spiff.com>
2023-12-11 13:58:45 -0500

    added moduledoc and typespec to cell struct

--- lib/xlsx_reader/cell.ex
+++ lib/xlsx_reader/cell.ex
@@ -1,3 +1,24 @@
 defmodule XlsxReader.Cell do
+  @moduledoc """
+  Cell structure.
+
+  This structure contains the information of a cell in a sheet.
+
+  - `value` - The value of the cell
+  - `formula` - The formula used in the cell, if any
+  - `ref` - The cell reference, like 'A1', 'B2', etc.
+
+  This structure is used when the `cell_data_format` option is set to `:cell`.
+  """
+
   defstruct [:value, :formula, :ref]
+
+  @typedoc """
+  XLSX cell data
+  """
+  @type t :: %__MODULE__{
+          value: term(),
+          formula: String.t() | nil,
+          ref: String.t()
+        }
 end
